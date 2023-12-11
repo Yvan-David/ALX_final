d96ebcc9f19836e713baea96308519329fe3f2ca
Yvan-David <irankundayvan2020@gmail.com>
2023-12-11 13:03:46 +0200

    home image

---
+++ lokobiz/src/components/Landing.jsx
@@ -0,0 +1,75 @@
+
+const Landing = () => {
+  
+  return (
+    <div className="landing-page">
+
+      <section className="landing">
+      <img src="./home-image.jpeg" alt="" />
+        <div className="landing-content">
+          <h1>DISCOVER AWESOME PLACES NEAR YOU</h1>
+          <div className="search-bar">
+
+          </div>
+          <div className="minimalcategories">
+            
+          </div>
+        </div>
+      </section>
+      {/* <main>
+        <div className="review">
+          <p>Testimonials</p>
+        </div>
+        <div className="review-container">
+          <div className="thomas">
+            <img
+              src="https://cdn.pixabay.com/photo/2017/08/01/01/33/beanie-2562646_1280.jpg"
+              alt="portait"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam ullam distinctio pariatur sint obcaecati, dolor quidem
+              omnis voluptatum rem explicabo. Odit rem iste vel cum quod,
+              blanditiis autem nobis sunt!
+            </p>
+            <h5>Thomas</h5>
+          </div>
+          <div className="emily">
+            <img
+              src="https://cdn.pixabay.com/photo/2018/05/06/03/39/woman-3377839_1280.jpg"
+              alt="portait"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam ullam distinctio pariatur sint obcaecati, dolor quidem
+              omnis voluptatum rem explicabo. Odit rem iste vel cum quod,
+              blanditiis autem nobis sunt!
+            </p>
+            <h5>Emily</h5>
+          </div>
+          <div className="jennie">
+            <img
+              src="https://cdn.pixabay.com/photo/2017/02/16/23/10/smile-2072907_1280.jpg"
+              alt="portait"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam ullam distinctio pariatur sint obcaecati, dolor quidem
+              omnis voluptatum rem explicabo. Odit rem iste vel cum quod,
+              blanditiis autem nobis sunt!
+            </p>
+            <h5>Jennie</h5>
+          </div>
+        </div>
+      </main> */}
+      {/* <footer>
+        <p>
+          &copy; <span class="data"> {new Date().getFullYear()}</span> Mending
+          Mind. All rights reserved.
+        </p>
+      </footer> */}
+    </div>
+  );
+};
+
+export default Landing;
